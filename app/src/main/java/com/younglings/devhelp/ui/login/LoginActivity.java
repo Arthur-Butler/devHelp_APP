8f4055bb0dbc34b4e98693a5df165f81a0256125
Arthur Butler <arthurbutler1738@gmail.com>
2022-02-11 15:19:26 +0200

    Changes to be committed: 	new file:   .idea/vcs.xml 	modified:   app/build.gradle 	new file:   app/src/main/java/com/younglings/devhelp/ui/login/EmailAndPasswordRegisterActivity.java 	modified:   app/src/main/java/com/younglings/devhelp/ui/login/LoginActivity.java 	new file:   app/src/main/java/com/younglings/devhelp/ui/login/MainActivity.java 	new file:   app/src/main/java/com/younglings/devhelp/ui/login/activity_email_and_password_register.java 	new file:   app/src/main/res/drawable/add_photo.png 	new file:   app/src/main/res/drawable/avatarr.png 	new file:   app/src/main/res/drawable/background.jpg 	new file:   app/src/main/res/drawable/ic_back.xml 	new file:   app/src/main/res/drawable/reg_edittext_style.xml 	new file:   app/src/main/res/drawable/sign_up_button.xml 	new file:   app/src/main/res/drawable/sign_up_ripple.xml 	new file:   app/src/main/res/font/impact.xml 	modified:   app/src/main/res/layout/activity_login.xml 	new file:   app/src/main/res/layout/fragment_activity_email_and_password_register.xml 	modified:   app/src/main/res/navigation/nav_graph.xml 	modified:   app/src/main/res/values/colors.xml 	modified:   app/src/main/res/values/strings.xml 	new file:   app/src/main/res/values/style.xml

--- app/src/main/java/com/younglings/devhelp/ui/login/LoginActivity.java
+++ app/src/main/java/com/younglings/devhelp/ui/login/LoginActivity.java
@@ -1,170 +1,124 @@
 package com.younglings.devhelp.ui.login;
 
-import android.app.Activity;
-
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
-import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.KeyEvent;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.util.Log;
-
 import com.younglings.devhelp.R;
-import com.younglings.devhelp.databinding.ActivityLoginBinding;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-//import com.google.firebase.database.Logger;
-//import com.google.firebase.referencecode.database.models.Post;
-//import com.google.firebase.referencecode.database.models.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
-    private static final String TAG = "ReadAndWriteSnippets";
-    //private DatabaseReference mDatabase;
-    private LoginViewModel loginViewModel;
-    private ActivityLoginBinding binding;
 
-    public void ReadAndWriteSnippets(DatabaseReference mDatabase) {
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-    };
+    private EditText userMail,userPassword;
+    private Button btnLogin;
+    private ProgressBar loginProgress;
+    private FirebaseAuth mAuth;
+    private Intent HomeActivity;
+    private TextView logToRegBtn;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
 
-        binding = ActivityLoginBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        loginViewModel = new ViewModelProvider(this, new LoginViewModelFactory())
-                .get(LoginViewModel.class);
-
-        private FirebaseAuth mAuth; mAuth = FirebaseAuth.getInstance();
-
-        NavHostFragment navHostFragment =
-                (NavHostFragment) supportFragmentManager.findFragmentById(R.id.nav_host_fragment);
-        NavController navController = navHostFragment.getNavController();
-
-        final EditText usernameEditText = binding.username;
-        final EditText passwordEditText = binding.password;
-        final Button loginButton = binding.login;
-        final ProgressBar loadingProgressBar = binding.loading;
-
-        public void onStart() {
-            super.onStart(){
-                FirebaseUser currentUser = mAuth.getCurrentUser();
-                if(currentUser != null){
-                    reload();
-            }
-        }
-
-        loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
-            @Override
-            public void onChanged(@Nullable LoginFormState loginFormState) {
-                if (loginFormState == null) {
-                    return;
-                }
-                loginButton.setEnabled(loginFormState.isDataValid());
-                if (loginFormState.getUsernameError() != null) {
-                    usernameEditText.setError(getString(loginFormState.getUsernameError()));
-                }
-                if (loginFormState.getPasswordError() != null) {
-                    passwordEditText.setError(getString(loginFormState.getPasswordError()));
-                }
-            }
-        });
-
-        loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
-            @Override
-            public void onChanged(@Nullable LoginResult loginResult) {
-                if (loginResult == null) {
-                    return;
-                }
-                loadingProgressBar.setVisibility(View.GONE);
-                if (loginResult.getError() != null) {
-                    showLoginFailed(loginResult.getError());
-                }
-                if (loginResult.getSuccess() != null) {
-                    updateUiWithUser(loginResult.getSuccess());
-                }
-                setResult(Activity.RESULT_OK);
-
-                //Complete and destroy login activity once successful
-                finish();
-            }
-        });
-
-        TextWatcher afterTextChangedListener = new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                // ignore
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                // ignore
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                loginViewModel.loginDataChanged(usernameEditText.getText().toString(),
-                        passwordEditText.getText().toString());
-            }
-        };
-        usernameEditText.addTextChangedListener(afterTextChangedListener);
-        passwordEditText.addTextChangedListener(afterTextChangedListener);
-        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    loginViewModel.login(usernameEditText.getText().toString(),
-                            passwordEditText.getText().toString());
-                }
-                return false;
-            }
-        });
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        logToRegBtn = findViewById(R.id.signUp);
+        logToRegBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loadingProgressBar.setVisibility(View.VISIBLE);
-                loginViewModel.login(usernameEditText.getText().toString(),
-                        passwordEditText.getText().toString());
+                Intent intent = new Intent(LoginActivity.this,EmailAndPasswordRegisterActivity.class);
+                startActivity(intent);
             }
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d(TAG, "Value is: " + usernameEditText.getText().toString());
-            }
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
+        });
+
+
+        userMail = findViewById(R.id.username);
+        userPassword = findViewById(R.id.password);
+        btnLogin = findViewById(R.id.login);
+        loginProgress = findViewById(R.id.loading);
+        mAuth = FirebaseAuth.getInstance();
+        HomeActivity = new Intent(this, MainActivity.class);
+
+
+
+        loginProgress.setVisibility(View.INVISIBLE);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginProgress.setVisibility(View.VISIBLE);
+                btnLogin.setVisibility(View.INVISIBLE);
+
+                final String mail = userMail.getText().toString();
+                final String password = userPassword.getText().toString();
+
+                if (mail.isEmpty() || password.isEmpty()) {
+                    showMessage("Please Verify All Field");
+                    btnLogin.setVisibility(View.VISIBLE);
+                    loginProgress.setVisibility(View.INVISIBLE);
+                }
+                else
+                {
+                    signIn(mail,password);
+                }
+
+
+
+
             }
         });
     }
 
-    private void updateUiWithUser(LoggedInUserView model) {
-        String welcome = getString(R.string.welcome) + model.getDisplayName();
-        // TODO : initiate successful logged in experience
-        Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
+    private void signIn(String mail, String password) {
+
+        mAuth.signInWithEmailAndPassword(mail,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+
+                if (task.isSuccessful()) {
+
+                    loginProgress.setVisibility(View.INVISIBLE);
+                    btnLogin.setVisibility(View.VISIBLE);
+                    updateUI();
+
+                }
+                else {
+                    showMessage(task.getException().getMessage());
+                    btnLogin.setVisibility(View.VISIBLE);
+                    loginProgress.setVisibility(View.INVISIBLE);
+                }
+
+
+            }
+        });
     }
 
-    private void showLoginFailed(@StringRes Integer errorString) {
-        Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
+    private void updateUI() {
+        startActivity(HomeActivity);
+        finish();
+
+    }
+
+    private void showMessage(String text) {
+        Toast.makeText(getApplicationContext(),text, Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if(user != null) {
+            //user is already connected  so we need to redirect him to home page
+            updateUI();
+        }
     }
 }
---
+++ app/src/main/java/com/younglings/devhelp/ui/login/MainActivity.java
@@ -0,0 +1,61 @@
+package com.younglings.devhelp.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.younglings.devhelp.R;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    FirebaseUser currentUser ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(Html.fromHtml("<font color=\"black\">"+getString(R.string.profile)+"</font>"));
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // ini
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+
+        userData();
+    }
+
+    public void userData() {
+        CircleImageView userImage = findViewById(R.id.imageView3);
+        TextView userName = findViewById(R.id.username);
+
+        userName.setText(currentUser.getDisplayName());
+
+        if (currentUser.getPhotoUrl() != null){
+            Glide.with(this).load(currentUser.getPhotoUrl()).into(userImage);
+        }
+        else {
+            Glide.with(this).load(R.drawable.avatarr).into(userImage);
+        }
+    }
+
+    public void logout(View view) {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
