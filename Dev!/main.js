9881ba9a001cfc21539b60dae2e58e71f61375b7
Zaakirah_Abrams <82881610+Zaaks-Projects@users.noreply.github.com>
2022-02-18 09:40:55 +0200

    Update on the landing page
    Footer needs to be added 
    The contact form needs to be connected to a database

---
+++ Dev!/main.js
@@ -0,0 +1,35 @@
+
+$('.features-car').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    dots:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:3,
+        }
+    }
+})
+$('.ui-display-car').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    dots:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:3,
+        },
+        1000:{
+            items:5,
+        }
+    }
+})
